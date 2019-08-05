0f6d648cd6b6695897fff595189a7c7467005f24
Jan Sakalos <sakalosj@gmail.com>
2019-08-05 21:01:02 +0200

    multiple album one2one relation fields handled

--- album/templatetags/album_tags.py
+++ album/templatetags/album_tags.py
@@ -31,18 +31,16 @@ def ct(format_string):
 
 class RenderAlbumNode(Node):
 
-    def __init__(self, album, group=None):
+    def __init__(self, album):
         super().__init__()
         self.album = album
-        self.group = group
         self.template_name = 'album/tag/album_detail_tag_template.html'
 
     def render(self, context):
         album = self.album.resolve(context)
-        group = self.group.resolve(context) if  self.group else '__all__'
 
         template = get_template(self.template_name)
-        return template.render(context={'imageList': album.get_images_by_group(group)})
+        return template.render(context={'album': album})
 
 
 @register.tag
@@ -51,8 +49,7 @@ def render_album(parser, token):
     bits.pop(0)  # tag_name
 
     album = parser.compile_filter(bits.pop(0))
-    group = parser.compile_filter(bits.pop(0)) if bits else None
 
     # tag_name, album, album_group = token.split_contents()
 
-    return RenderAlbumNode(album, group)
+    return RenderAlbumNode(album)
